1d51e25efcc57f595d968c957a0dfd620468e911
Raimond Gaev <raimond.gaev@eficode.com>
2023-01-10 14:23:30 +0200

    with bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,6 +32,6 @@ router.get('/api/weather', async ctx => {
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(port);
+app.listen(portt);
 
 console.log(`App listening on port ${port}`);
